b56b5d098572f2bbe3a0654bb501a7e9755d5cd6
FacundoBe <facundobellomo@gmail.com>
2026-02-09 01:01:24 -0300

    listo obtener datos de byma de precio

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,52 @@
 import { useEffect, useState } from 'react'
 import { getBymaData } from './services/onData'
 import { calculaRendimientoOnPuente } from './services/calculadoraOnPuente'
+import { onsAAA } from './JSONS/OnsAAA'
 import './App.css'
 
 function App() {
 
   const [tir, setTir] = useState(0)
- const [price, setPrice] = useState(0)
+  const [price, setPrice] = useState(0)
+  const [bymaOnsData, setBymaOnsData] = useState([])
 
-  useEffect(() => {
-    const videos = getBymaData()
-      .then(bymaData => {console.log(bymaData.filter( papel => papel.symbol ===  "IRCFD" ))
-                                                      setPrice( bymaData.filter( papel => papel.symbol ===  "IRCFD" )[0].trade)}
+  function obtenerDatosDeByma() {
+    getBymaData()
+      .then(bymaData => {
+        console.log("logueando", bymaData)
+        if (bymaData.length > 0) setBymaOnsData(bymaData)
+        else console.log("No se pudo obtener los datos de precios y volumenes de BYMA")
+      }
+      )
+  }
 
-    )
+  function obtenerPreciosDelaOn(on) {
+    // on es un String con el tiker de la obligacion negociable de la que se desea obtener los precios 
+    const onData = bymaData.filter(papel => papel?.symbol === on)[0]
+    return { ultimoPrecio: onData.price, precioCompra: onData.bidPrice, precioVenta: onData.offerPrice}
+  }
 
-  }, [])
 
-  const priceFormated = price.toString().replaceAll(".", ","); 
+
+
+  const priceFormated = price.toString().replaceAll(".", ",");
   console.log(priceFormated)
 
   async function getTir() {
     const tir = await calculaRendimientoOnPuente("IRCFO", priceFormated)
     console.log(tir)
-    if (tir)setTir(tir)
+    if (tir) setTir(tir)
   }
 
   return (
     <>
 
       <p className="read-the-docs">
-        {`Tir  ${tir}%    ${price}`  } 
+        {`Tir  ${tir}%    ${price}`}
       </p>
-
-      <button type='button' className="manage-history-buttons" onClick={() => getTir() } > Calculate Tir </button>
+      <button type='button' className="manage-history-buttons" onClick={() => obtenerDatosDeByma()} >Obtener precios On de BYMA </button>
+      <br />
+      <button type='button' className="manage-history-buttons" onClick={() => getTir()} > Calculate Tir </button>
 
     </>
   )
